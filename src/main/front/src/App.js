2d778267e87668e8c7e0c5a546c734875ee447ad
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-01-23 14:33:43 +0900

    react 기본설정
    20230123 react 기본설정

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,33 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
 function App() {
-
-    const baseUrl = "http://localhost:8080";
-
-    const [ data, setData ] = useState('');
-
-    useEffect(() => { // 컴포넌트가 마운트 될 때 실행
-        springDataSet();
-    },[])
-
-    async function springDataSet() { // Axios 방식 사용
-        await axios
-            .get(baseUrl + "/data-test") // 해당 URL에 HTTP GET 요청
-            .then((res)=>{
-                console.log(res);
-                setData(res.data); // GET 요청하여 응답받은 data
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-    }
-
-    return (
-        <div className="App">
-            <h1>{data}</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1></h1>
+    </div>
+  );
 }
 
 export default App;
