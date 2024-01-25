6217c6c2f61519e5d16269ac24a427d37e156c53
LeeTaeSuk <twohugerock@gmail.com>
2024-01-25 15:31:31 +0900

    240125 게시판 구현 완료(설계 구현은 완료 되었지만 리액트랑 합치고 나서는 수정 필요)

---
+++ src/main/java/com/example/member_coummunity_board/Controller/BoardController.java
@@ -0,0 +1,55 @@
+package com.example.member_coummunity_board.Controller;
+
+import com.example.member_coummunity_board.DTO.BoardListView;
+import com.example.member_coummunity_board.DTO.BoardViewResponse;
+import com.example.member_coummunity_board.Domain.Board;
+import com.example.member_coummunity_board.Service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    //게시물 리스트로 보여줌
+    @GetMapping("boards")
+    public String getBoards(Model model){
+        List<BoardListView> boards = boardService.findAll()
+                .stream()
+                .map(BoardListView::new)
+                .toList();
+        model.addAttribute("boards",boards);
+        return "boardList";
+    }
+
+    //게시물 세부내용
+    @GetMapping("/articles/{id}")
+    public String getBoard(@PathVariable Long id, Model model){
+        Board board = boardService.findById(id);
+        model.addAttribute("article", new BoardListView(board));
+
+        return "board";
+    }
+
+//새 게시물 작성
+    @GetMapping("/new-article")
+    public String newBoard(@RequestParam(required = false) Long id, Model model){
+
+        if(id == null) {
+            model.addAttribute("article", new BoardViewResponse());
+        } else {
+            Board board = boardService.findById(id);
+            model.addAttribute("article", new BoardViewResponse(board));
+        }
+
+        return "newArticle";
+    }
+
+
+
+}
