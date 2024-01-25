5746300b65486df85e3dd223d567e26efdee6cc2
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-01-25 15:33:24 +0900

    20240125 홈 화면 ui 구성
    홈 화면 ui 구성

--- src/main/front/src/components/Skeleton.jsx
+++ src/main/front/src/components/Skeleton.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 const Skeleton = () => {
   return (
     <>
-      <div className="flex flex-wrap -mx-3 w-full ">
+      <div className="flex flex-wrap -mx-3 w-full drop-shadow-sm mx-auto ">
         <div className="px-20 mb-1 mx-auto w-full bg-white border-gray-200">
-          <div className="sm:flex items-center sitems-stretch justify-between grow lg:mb-0 py-4 px-5">
+          <div className="sm:flex items-center sitems-stretch justify-between grow lg:mb-0 py-4 px-5 max-w-5xl mx-auto">
             <div className="flex flex-col flex-wrap justify-center mb-5 mr-3 lg:mb-0">
               <span className="my-0 flex text-dark font-semibold text-[1.35rem]/[1.2] flex-col justify-center">
                 밀키웨이 🌠
@@ -13,7 +13,19 @@ const Skeleton = () => {
             </div>
             <div className="flex items-center lg:shrink-0 lg:flex-nowrap">
               <div className="relative flex items-center lg:ml-4 sm:mr-0 mr-2">
-                userName
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="ml-2">user</span>
               </div>
             </div>
           </div>
