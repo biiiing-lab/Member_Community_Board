b6b57c95edb16852fe0730e87b8602f57aec2f22
bibi <youbinking@naver.com>
2024-01-30 08:40:39 +0900

    240130 token service

---
+++ src/main/java/com/example/member_coummunity_board/Service/TokenService.java
@@ -0,0 +1,9 @@
+package com.example.member_coummunity_board.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+}
