b4799b328cb22b14f8c22325593c2920ded0b01a
bibi <youbinking@naver.com>
2024-01-30 11:55:25 +0900

    240130 delete files

--- src/main/java/com/example/member_coummunity_board/Repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.member_coummunity_board.Repository;
-
-import com.example.member_coummunity_board.Entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-    User findByMember(String memberId);
-}
--- src/main/java/com/example/member_coummunity_board/Service/TokenService.java
+++
@@ -1,9 +0,0 @@
-package com.example.member_coummunity_board.Service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Service
-public class TokenService {
-}
