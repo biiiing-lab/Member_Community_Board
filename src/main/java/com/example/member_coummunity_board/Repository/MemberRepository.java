a9e4f3ec9f5c910baa70941816d932fcf86d7a2d
bibi <youbinking@naver.com>
2024-01-30 11:56:27 +0900

    240130 repository class 재생성

---
+++ src/main/java/com/example/member_coummunity_board/Repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.member_coummunity_board.Repository;
+import com.example.member_coummunity_board.Entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+    User findByMember(String memberId);
+}
