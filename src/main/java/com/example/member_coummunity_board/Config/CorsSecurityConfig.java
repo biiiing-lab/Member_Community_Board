a4f6e8c33710985ff4b7d4e601717f8162f69961
bibi <youbinking@naver.com>
2024-02-01 11:47:12 +0900

    240201 cors 수정

--- src/main/java/com/example/member_coummunity_board/Config/CorsSecurityConfig.java
+++ src/main/java/com/example/member_coummunity_board/Config/CorsSecurityConfig.java
@@ -2,10 +2,13 @@ package com.example.member_coummunity_board.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -13,9 +16,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsSecurityConfig {
 
     @Bean // 전역적 처리
@@ -41,7 +43,9 @@ public class CorsSecurityConfig {
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
         source.registerCorsConfiguration("/**",config);
+
         return new CorsFilter(source);
     }
 }
