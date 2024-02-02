bf7f1c329d0450c868021421090008c5ebcdf045
LeeTaeSuk <twohugerock@gmail.com>
2024-02-02 18:43:54 +0900

    240202 홈페이지 리스트 번호, 작성자, 작성일 출력 완료

--- src/main/java/com/example/member_coummunity_board/Repository/MemberRepository.java
+++ src/main/java/com/example/member_coummunity_board/Repository/MemberRepository.java
@@ -4,7 +4,6 @@ import com.example.member_coummunity_board.Domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, String> {
     MemberResponseDto findByMemberIdAndPassword(String memberId, String password);
-
 }
